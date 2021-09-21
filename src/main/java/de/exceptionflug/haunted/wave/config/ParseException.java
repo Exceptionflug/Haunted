2479cd3230e7717fb0b13956075f5f3e440a5ee1
Nico <n.britze@exceptionflug.de>
2021-09-21 14:19:03 +0200

    Optimized ParseException

--- src/main/java/de/exceptionflug/haunted/wave/config/ParseException.java
+++ src/main/java/de/exceptionflug/haunted/wave/config/ParseException.java
@@ -1,9 +1,5 @@
 package de.exceptionflug.haunted.wave.config;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Date: 15.08.2021
  *
@@ -13,12 +9,14 @@ public class ParseException extends Exception {
 
     private final String fileName;
     private final int line;
+    private boolean b0;
 
     public ParseException(String message, Exception cause, String fileName, int line) {
         super(message, cause);
         this.fileName = fileName;
         this.line = line;
     }
+
     public ParseException(String message, String fileName, int line) {
         super(message);
         this.fileName = fileName;
@@ -26,11 +24,37 @@ public class ParseException extends Exception {
     }
 
     @Override
-    public StackTraceElement[] getStackTrace() {
-        List<StackTraceElement> stackTraceElements = new ArrayList<>();
-        stackTraceElements.add(new StackTraceElement(fileName.substring(0, fileName.lastIndexOf(".")), "init", fileName, line));
-        stackTraceElements.addAll(Arrays.asList(super.getStackTrace()));
-        return stackTraceElements.toArray(new StackTraceElement[0]);
+    public String getMessage() {
+        if (b0) {
+            return super.getMessage();
+        }
+        StringBuilder builder = new StringBuilder(super.getMessage() + " (" + fileName + ":" + line + ")\n");
+        for (Throwable cause : getSuppressed()) {
+            if (cause instanceof ParseException e) {
+                ((ParseException) cause).b0 = true;
+                builder.append("-> ").append(e.getMessage()).append("\n");
+            } else {
+                builder.append("-> ").append(cause).append("\n");
+            }
+            if (cause.getCause() != null) {
+                examineCause(cause.getCause(), builder);
+            }
+        }
+        builder.append("\n");
+        builder.append("For more details, see the following stacktrace:");
+        return builder.toString();
+    }
+
+    private void examineCause(Throwable cause, StringBuilder builder) {
+        if (cause instanceof ParseException e) {
+            e.b0 = true;
+            builder.append("  - ").append(e.getMessage()).append("\n");
+        } else {
+            builder.append("  - ").append(cause).append("\n");
+        }
+        if (cause.getCause() != null) {
+            examineCause(cause.getCause(), builder);
+        }
     }
 
 }
